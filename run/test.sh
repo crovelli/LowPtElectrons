facaaa8f7387c363b9fe2ffcad486d7c853f9d8d
Robert Bainbridge <bainbrid@users.noreply.github.com>
2018-11-22 16:45:34 +0000

    triv

--- run/test.sh
+++ run/test.sh
@@ -2,8 +2,8 @@ OUTPUT=track_features.root
 FAKES=-1
 EDOUT=0
 EVENTS=1
+INPUT_FILES=root://cms-xrd-global.cern.ch//store/cmst3/group/bpark/BToKee_Pythia_PUMix_18_03_18_180318_112206_0000/BToKee_PUMix_10.root
 
-export INPUT_FILES=root://cms-xrd-global.cern.ch//store/cmst3/group/bpark/BToKee_Pythia_PUMix_18_03_18_180318_112206_0000/BToKee_PUMix_10.root
 cmsRun mc_features.py \
     inputFiles=$INPUT_FILES \
     outname=$OUTPUT \
