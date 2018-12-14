6c97e305dd5c541deede3fb96e73447c392bca27
mauro verzetti <mverzett@cern.ch>
2018-12-14 16:47:56 +0100

    change path to electron gun

--- python/samples/__init__.py
+++ python/samples/__init__.py
@@ -5,7 +5,7 @@ all_samples = {
    'fill6371AOD' : fill6371AOD.input_files,
    'RAWBlocks' : RAWBlocks.input_files,
    'RAWTest' : ['/store/data/Run2017F/SingleElectron/RAW/v1/000/306/459/00000/B6410DA6-C8C5-E711-947F-02163E01A45E.root'],
-   'EleGunTest' : ['file:/afs/cern.ch/work/m/mverzett/public/singleEle_Pt0To10_EBOnly_noPU_RAW.root'],
+   'EleGunTest' : ['file:/afs/cern.ch/work/m/mverzett/public/ELEGUN_GEN-SIM-RAW.root'],
    'RAWMC' : BtoKeeRAW.all_files,
    'RAWMCTest' : BtoKeeRAW.test_file
 }
