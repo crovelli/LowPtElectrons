c5dfab043df7ce782c84c9504c62e4360629daf7
crovelli <crovelli@gmail.com>
2020-11-27 16:07:23 +0100

    port code to 10.6.x; remove unused features; read all needed infos from EGM class

--- run/slimntuplizer_cfg.py
+++ run/slimntuplizer_cfg.py
@@ -2,14 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 from FWCore.ParameterSet.VarParsing import VarParsing
 options = VarParsing('python')
-options.setDefault('maxEvents',100)
+options.setDefault('maxEvents',-1)
 options.register('skipEvents',0,VarParsing.multiplicity.singleton,VarParsing.varType.int,"")
-options.register('useAOD',False,VarParsing.multiplicity.singleton,VarParsing.varType.bool,"")
+options.register('useAOD',True,VarParsing.multiplicity.singleton,VarParsing.varType.bool,"")
 options.parseArguments()
 
 process = cms.Process('TEST')
 
-default_file = 'root://xrootd-cms.infn.it//store/mc/RunIIAutumn18MiniAOD/DoubleElectron_FlatPt-HalfTo50/MINIAODSIM/FlatPU0to70IdealECALforBParking_102X_upgrade2018_realistic_v15-v2/250000/186F3612-08B6-4342-9F1B-23646A238BFE.root'
+default_file = 'root://xrootd-cms.infn.it//store//mc/RunIISummer19UL17RECO/BuToKJpsi_Toee_Mufilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/AODSIM/TrkExtra_106X_mc2017_realistic_v6-v1/10000/01ADC5E2-1205-0749-8DFF-E896012FB20E.root'
 
 output_file = 'output_fromAOD_slim.root' if options.useAOD else 'output_fromMINIAOD_slim.root'
 
@@ -36,13 +36,12 @@ else :           switchOnVIDElectronIdProducer(process,DataFormat.MiniAOD)
 for idmod in ['RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_noIso_V2_cff'] :
     setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
 
-
 process.ntuplizer_seq = cms.Sequence()
 
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 500
 
-process.load('RecoEgamma.EgammaElectronProducers.lowPtGsfElectronID_cff')
+process.load('RecoEgamma.EgammaElectronProducers.lowPtGsfElectronID_cfi')
 if not options.useAOD : 
     process.lowPtGsfElectronID.electrons = 'slimmedLowPtElectrons'
     process.lowPtGsfElectronID.rho = 'fixedGridRhoFastjetAll'
@@ -51,106 +50,41 @@ process.ntuplizer_seq *= process.lowPtGsfElectronID
 process.load("Configuration.Geometry.GeometryRecoDB_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '102X_upgrade2018_realistic_v15')
-# this is for the LowPt energy regression
+process.GlobalTag = GlobalTag(process.GlobalTag, '106X_mc2017_realistic_v6')
+
 process.GlobalTag.toGet = cms.VPSet(
 cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_eb_ecalOnly_05To20_mean"),
-         tag = cms.string("lowPtElectron_eb_ecalOnly_05To20_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
+         label = cms.untracked.string("lowPtElectron_eb_ecalOnly_05To50_mean"),
+         tag = cms.string("lowPtElectron_eb_ecalOnly_05To50_mean_2017UL"),
+         connect = cms.string("sqlite_file:lowPtEleReg_2017UL_25112020.db")),
 cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_ee_ecalOnly_05To20_mean"),
-         tag = cms.string("lowPtElectron_ee_ecalOnly_05To20_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
+         label = cms.untracked.string("lowPtElectron_ee_ecalOnly_05To50_mean"),
+         tag = cms.string("lowPtElectron_ee_ecalOnly_05To50_mean_2017UL"),
+         connect = cms.string("sqlite_file:lowPtEleReg_2017UL_25112020.db")),
 cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_eb_ecalOnly_05To20_sigma"),
-         tag = cms.string("lowPtElectron_eb_ecalOnly_05To20_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
+         label = cms.untracked.string("lowPtElectron_eb_ecalOnly_05To50_sigma"),
+         tag = cms.string("lowPtElectron_eb_ecalOnly_05To50_sigma_2017UL"),
+         connect = cms.string("sqlite_file:lowPtEleReg_2017UL_25112020.db")),
 cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_ee_ecalOnly_05To20_sigma"),
-         tag = cms.string("lowPtElectron_ee_ecalOnly_05To20_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
+         label = cms.untracked.string("lowPtElectron_ee_ecalOnly_05To50_sigma"),
+         tag = cms.string("lowPtElectron_ee_ecalOnly_05To50_sigma_2017UL"),
+         connect = cms.string("sqlite_file:lowPtEleReg_2017UL_25112020.db")),
 cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_eb_ecalTrk_05To20_mean"),
-         tag = cms.string("lowPtElectron_eb_ecalTrk_05To20_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
+         label = cms.untracked.string("lowPtElectron_eb_ecalTrk_05To50_mean"),
+         tag = cms.string("lowPtElectron_eb_ecalTrk_05To50_mean_2017UL"),
+         connect = cms.string("sqlite_file:lowPtEleReg_2017UL_25112020.db")),
 cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_ee_ecalTrk_05To20_mean"),
-         tag = cms.string("lowPtElectron_ee_ecalTrk_05To20_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
+         label = cms.untracked.string("lowPtElectron_ee_ecalTrk_05To50_mean"),
+         tag = cms.string("lowPtElectron_ee_ecalTrk_05To50_mean_2017UL"),
+         connect = cms.string("sqlite_file:lowPtEleReg_2017UL_25112020.db")),
 cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_eb_ecalTrk_05To20_sigma"),
-         tag = cms.string("lowPtElectron_eb_ecalTrk_05To20_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
+         label = cms.untracked.string("lowPtElectron_eb_ecalTrk_05To50_sigma"),
+         tag = cms.string("lowPtElectron_eb_ecalTrk_05To50_sigma_2017UL"),
+         connect = cms.string("sqlite_file:lowPtEleReg_2017UL_25112020.db")),
 cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_ee_ecalTrk_05To20_sigma"),
-         tag = cms.string("lowPtElectron_ee_ecalTrk_05To20_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")), 
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_eb_ecalOnly_20To50_mean"),
-         tag = cms.string("lowPtElectron_eb_ecalOnly_20To50_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_ee_ecalOnly_20To50_mean"),
-         tag = cms.string("lowPtElectron_ee_ecalOnly_20To50_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_eb_ecalOnly_20To50_sigma"),
-         tag = cms.string("lowPtElectron_eb_ecalOnly_20To50_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_ee_ecalOnly_20To50_sigma"),
-         tag = cms.string("lowPtElectron_ee_ecalOnly_20To50_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_eb_ecalTrk_20To50_mean"),
-         tag = cms.string("lowPtElectron_eb_ecalTrk_20To50_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_ee_ecalTrk_20To50_mean"),
-         tag = cms.string("lowPtElectron_ee_ecalTrk_20To50_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_eb_ecalTrk_20To50_sigma"),
-         tag = cms.string("lowPtElectron_eb_ecalTrk_20To50_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("lowPtElectron_ee_ecalTrk_20To50_sigma"),
-         tag = cms.string("lowPtElectron_ee_ecalTrk_20To50_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")), 
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("gsfElectron_eb_ecalOnly_05To50_mean"),
-         tag = cms.string("gsfElectron_eb_ecalOnly_05To50_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("gsfElectron_ee_ecalOnly_05To50_mean"),
-         tag = cms.string("gsfElectron_ee_ecalOnly_05To50_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("gsfElectron_eb_ecalOnly_05To50_sigma"),
-         tag = cms.string("gsfElectron_eb_ecalOnly_05To50_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("gsfElectron_ee_ecalOnly_05To50_sigma"),
-         tag = cms.string("gsfElectron_ee_ecalOnly_05To50_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("gsfElectron_eb_ecalTrk_05To50_mean"),
-         tag = cms.string("gsfElectron_eb_ecalTrk_05To50_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("gsfElectron_ee_ecalTrk_05To50_mean"),
-         tag = cms.string("gsfElectron_ee_ecalTrk_05To50_mean_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("gsfElectron_eb_ecalTrk_05To50_sigma"),
-         tag = cms.string("gsfElectron_eb_ecalTrk_05To50_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")),
-cms.PSet(record = cms.string("GBRDWrapperRcd"),
-         label = cms.untracked.string("gsfElectron_ee_ecalTrk_05To50_sigma"),
-         tag = cms.string("gsfElectron_ee_ecalTrk_05To50_sigma_2018V1"),
-         connect = cms.string("sqlite_file:lowPtEleReg_2018_02062020_nv.db")))
-
+         label = cms.untracked.string("lowPtElectron_ee_ecalTrk_05To50_sigma"),
+         tag = cms.string("lowPtElectron_ee_ecalTrk_05To50_sigma_2017UL"),
+         connect = cms.string("sqlite_file:lowPtEleReg_2017UL_25112020.db")))
 
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
